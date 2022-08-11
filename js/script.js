e41bf834e1a19b02586559b4854ac11403b827be
Baghdadi Mustapha <mrmusito@gmail.com>
2022-08-11 14:56:24 +0200

    not sure what happened but lost all js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
 const trip = document.getElementById("trip");
 trip.addEventListener("mouseenter", function(event) {
     this.classList.add("bigger")
-    // console.log(event)
 });
 trip.addEventListener("mouseleave", function(event) {
     this.classList.remove("bigger")
-    // console.log(event)
-})
+});
 const book = document.getElementById("book");
 book.addEventListener("mouseenter", function(event) {
     this.classList.add("bigger")
-    // console.log(event)
 });
 book.addEventListener("mouseleave", function(event) {
     this.classList.remove("bigger")
-    // console.log(event)
-})
+});
 const movie = document.getElementById("movie");
 movie.addEventListener("mouseenter", function(event) {
     this.classList.add("bigger")
-    // console.log(event)
 });
 movie.addEventListener("mouseleave", function(event) {
     this.classList.remove("bigger")
-    // console.log(event)
-})
+});
 const music = document.getElementById("music");
 music.addEventListener("mouseenter", function(event) {
     this.classList.add("bigger")
-    // console.log(event)
 });
 music.addEventListener("mouseleave", function(event) {
     this.classList.remove("bigger")
-    // console.log(event)
-})
-
-  
+});
